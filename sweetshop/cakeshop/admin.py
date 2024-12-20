8d113de1087b6dfe8d6ee5ff81c6736286a7f4c2
Kirill-Alexeev <kirushkalexeev@gmail.com>
2024-12-21 01:13:59 +0300

    created models, admin, added images, records in Cakes and Ingredients

--- sweetshop/cakeshop/admin.py
+++ sweetshop/cakeshop/admin.py
@@ -1,3 +1,78 @@
 from django.contrib import admin
+from .models import Cake, Order, Review, Ingredient, OrderCake
 
-# Register your models here.
+
+class IngredientInline(admin.TabularInline):
+    model = Cake.ingredients.through
+    extra = 0
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 0
+
+
+class OrderCakeInline(admin.TabularInline):
+    model = OrderCake
+    extra = 0
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'units', 'count', 'created_at', 'updated_at')
+    fields = ['id', 'title', 'units', 'count', ('created_at', 'updated_at')]
+    readonly_fields = ('id', 'created_at', 'updated_at')
+    search_fields = ('title',)
+    list_filter = ('units',)
+    list_per_page = 15
+
+
+@admin.register(Cake)
+class CakeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'weight', 'price', 'created_at', 'updated_at')
+    fields = ['id', 'title', 'weight', 'description', 'image', 'price', ('created_at', 'updated_at')]
+    readonly_fields = ('id', 'created_at', 'updated_at')
+    search_fields = ('title',)
+    list_filter = ('title', 'price',)
+    filter_horizontal = ('ingredients',)
+    date_hierarchy = 'created_at'
+    inlines = [IngredientInline, ReviewInline]
+    list_per_page = 10
+
+    @admin.display(description="Количество символов", ordering='description')
+    def brief_info(self, cake: Cake):
+        return f"Описание содержит {len(cake.description)} символов."
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'cake', 'review', 'created_at')
+    fields = ['id', 'user', 'cake', 'review', 'created_at']
+    readonly_fields = ('id', 'created_at')
+    date_hierarchy = 'created_at'
+    search_fields = ('review',)
+    raw_id_fields = ('user', 'cake')
+    list_per_page = 10
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'execution_date', 'status', 'cost', 'delivery_address', 'created_at', 'updated_at')
+    list_display_links = ('id', 'user')
+    fields = ['id', 'user', 'execution_date', 'status', 'cost', 'delivery_address', ('created_at', 'updated_at')]
+    readonly_fields = ('id', 'created_at', 'updated_at')
+    search_fields = ('user__username', 'delivery_address')
+    list_filter = ('status', 'execution_date')
+    date_hierarchy = 'created_at'
+    inlines = [OrderCakeInline]
+    list_per_page = 10
+
+
+@admin.register(OrderCake)
+class OrderCakeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'cake')
+    fields = ['id', 'order', 'cake']
+    readonly_fields = ('id',)
+    raw_id_fields = ('order', 'cake')
+    search_fields = ('order__id', 'cake__title')
+    list_per_page = 15
--- sweetshop/cakeshop/models.py
+++ sweetshop/cakeshop/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True, help_text="Уникальный ID для ингредиента")
+    title = models.CharField(verbose_name='Название', max_length=100, help_text="Название ингредиента")
+    units = models.CharField(verbose_name='Единицы измерения', max_length=10, help_text="Наименование единиц измерения")
+    count = models.IntegerField(verbose_name='Количество', help_text="Количество ингредиента")
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateField(verbose_name='Дата обновления', auto_now=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("ingredient-detail", args=[str(self.id)])
+
+
+class Cake(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True, help_text="Уникальный ID для торта")
+    title = models.CharField(verbose_name='Название', max_length=100, help_text="Название торта")
+    weight = models.IntegerField(verbose_name='Вес', help_text="Вес торта в граммах")
+    description = models.TextField(verbose_name='Описание', max_length=1000, help_text="Описание торта")
+    image = models.ImageField(verbose_name='Изображение', upload_to="cakeshop/static/img/", help_text="Изображение торта")
+    price = models.DecimalField(verbose_name='Цена', max_digits=10, decimal_places=2, help_text="Цена торта в руб.")
+    ingredients = models.ManyToManyField(Ingredient, verbose_name='Ингредиенты', help_text="Ингредиенты используемые для приготовления")
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateField(verbose_name='Дата обновления', auto_now=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("cake-detail", args=[str(self.id)])
+
+
+class Review(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True, help_text="Уникальный ID для отзыва")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', help_text="Пользователь, оставивший отзыв")
+    cake = models.ForeignKey(Cake, on_delete=models.CASCADE, verbose_name='Торт', help_text="Торт, на который оставили отзыв")
+    review = models.TextField(verbose_name='Отзыв', max_length=1000, help_text="Текст отзыва")
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
+
+    class Meta:
+        ordering = ['cake']
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_absolute_url(self):
+        return reverse("review-detail", args=[str(self.id)])
+
+
+class Order(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True, help_text="Уникальный ID для заказа")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', help_text="")
+    execution_date = models.DateField(verbose_name='Дата доставки', help_text="Дата доставки заказа клиенту")
+    status = models.CharField(verbose_name='Статус', max_length=1, choices=[('О', 'Ожидается'), ('З', 'Завершён')], help_text="Статус заказа")
+    cost = models.DecimalField(verbose_name='Стоимость', max_digits=10, decimal_places=2, help_text="Общая стоимость заказа")
+    cakes = models.ManyToManyField(Cake, verbose_name='Торты', through="OrderCake", help_text="Торты добавленные в заказ")
+    delivery_address = models.CharField(verbose_name='Адрес доставки', max_length=255, help_text="Адрес, по которому доставляется заказ")
+    created_at = models.DateField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateField(verbose_name='Дата обновления', auto_now=True)
+
+    class Meta:
+        ordering = ["id"]
+
+    def __str__(self):
+        return f"Заказ #{self.id} для {self.user.username}"
+
+
+class OrderCake(models.Model):
+    id = models.IntegerField(primary_key=True, auto_created=True, help_text="Уникальный ID")
+    order = models.ForeignKey(Order, verbose_name='Заказ', on_delete=models.CASCADE)
+    cake = models.ForeignKey(Cake, verbose_name='Торт', on_delete=models.CASCADE)
